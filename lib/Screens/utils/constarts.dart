3368bc4305741ac6f832d017bf6a385f0927a91c
james <kamirijames11@gmail.com>
2023-12-10 19:28:30 +0300

    live change state

--- lib/Screens/utils/constarts.dart
+++ lib/Screens/utils/constarts.dart
@@ -1,9 +1,9 @@
-String appApiUrl='http://10.0.2.2:8000/api';
-String fileAppApiUrl='http://10.0.2.2:8000/';
+// String appApiUrl='http://10.0.2.2:8000/api';
+// String fileAppApiUrl='http://10.0.2.2:8000/';
 
 
-// String appApiUrl='https://learnstar.deepnet.co.ke/api';
-// String fileAppApiUrl='https://learnstar.deepnet.co.ke/';
+String appApiUrl='https://learnstar.deepnet.co.ke/api';
+String fileAppApiUrl='https://learnstar.deepnet.co.ke/';
 
 
 
